8ca3051e0c0a081500a80301c227b1209a6318b9
John Pekcan <johnpekcan@gmail.com>
2025-02-21 22:17:29 -0800

    multigrep

--- init.lua
+++ init.lua
@@ -415,12 +415,17 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            theme = 'ivy',
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
         },
+        require('custom.plugins.multigrep').setup(),
       }
 
       -- Enable Telescope extensions if they are installed
